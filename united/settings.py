359266b74d66f18b6cb5d7eb2997d2a2e2fdb275
Saif199001 <zeeshais8@gmail.com>
2026-04-09 11:01:58 +0530

    Fix Static Files for Admin

--- united/settings.py
+++ united/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'united.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -172,6 +172,7 @@ cloudinary.config(
 )
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
